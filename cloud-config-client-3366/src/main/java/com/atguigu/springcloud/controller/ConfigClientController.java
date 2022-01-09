3c70e66a17730febded6d0bc5c1f0234de3c9138
pengtao4560 <pengtao4560@sina.com>
2022-01-09 12:45:06 +0800

    feat:P82 bug动态刷新定点通知      P81 Bus动态刷新全局广播配置实现      P80 Bus动态刷新全局广播的设计思想      P79 Bus之RabbitMQ环境配置      P78 Bus消息总线是什么

---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @auther pengtao
+ * @create 2021年12月21日22:52:42
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo()
+    {
+        return configInfo;
+    }
+}
