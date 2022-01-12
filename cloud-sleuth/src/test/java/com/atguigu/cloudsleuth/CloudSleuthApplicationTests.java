e29ffad98c709cd2ee9a696a6a41559c3b9f94de
pengtao4560 <pengtao4560@sina.com>
2022-01-12 21:20:19 +0800

    feat: p95 cloud alibaba简介

---
+++ cloud-sleuth/src/test/java/com/atguigu/cloudsleuth/CloudSleuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.cloudsleuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudSleuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
