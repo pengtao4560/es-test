3dc453f0aa953eb05f846a0d198e1eb35fde364d
pengtao <pengtao4560@sina.com>
2022-01-19 17:26:07 +0800

    feat: 迁移工作的配置 研究 openFeign超时时间 单独某一个请求

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/config/FeignConfig.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/config/FeignConfig.java
@@ -1,6 +1,8 @@
 package com.atguigu.springcloud.config;
 
+import feign.Feign;
 import feign.Logger;
+import feign.Request;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,4 +20,11 @@ public class FeignConfig {
         // CTRL 查看 Logger.Level
         return Logger.Level.FULL;
     }
+    @Bean
+    Request.Options getOption(){
+        Request.Options options = new Request.Options(3500, 3500);
+        return options;
+    }
+
+
 }
--- cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/service/PaymentFeignService.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/service/PaymentFeignService.java
@@ -1,5 +1,6 @@
 package com.atguigu.springcloud.service;
 
+import com.atguigu.springcloud.config.FeignConfig;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @date 2021/11/30 0030 - 20:57
  */
 @Component
-@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE", configuration = FeignConfig.class)
 public interface PaymentFeignService {
     @GetMapping(value = "/payment/get/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
