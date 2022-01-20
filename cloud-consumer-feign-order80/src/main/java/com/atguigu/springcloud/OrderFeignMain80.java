c2cab2e2c1888b447019b4383193a6d859ea2a89
pengtao <pengtao4560@sina.com>
2022-01-20 16:27:49 +0800

    TODO: openFeign怎么为单独某一个url配置不同于全局配置的超时时间，未解决

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 public class OrderFeignMain80 {
+
     public static void main(String[] args) {
         SpringApplication.run(OrderFeignMain80.class, args);
     }
