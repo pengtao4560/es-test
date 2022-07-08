38113557606ddab3856746eac3b2f483407522ba
pengtao <pengtao4560@sina.com>
2022-07-08 17:28:56 +0800

    给自己立的flag

--- cloud-api-commons/src/main/test/com/atguigu/springcloud/hutooltest/PtTest.java
+++ cloud-api-commons/src/main/test/com/atguigu/springcloud/hutooltest/PtTest.java
@@ -7,5 +7,7 @@ public class PtTest {
     public static void main(String[] args) {
         System.out.println(11.5 * 1.3);
         System.out.println(15 * 12);
+        System.out.println();
+        System.out.println((1.5 * 5 + 3 * 2) / 7.0);
     }
 }
